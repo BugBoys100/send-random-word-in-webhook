987a06b3e98854956d12c329548380aaf2253ac1
Bug Boys <72216345+BugBoys100@users.noreply.github.com>
2022-02-01 09:23:51 +0100

    Non testé

--- larousse.py
+++ larousse.py
@@ -39,4 +39,31 @@ def word_in_webhook(nombre:int, definition:bool):
             resultat = resultat + \
             f'\n- **__{mots_a_trouver[i].capitalize()} : https://www.larousse.fr/dictionnaires/francais/{mots_a_trouver[i]}__** \n'
 
-    return resultat
+        headers = {
+            "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+        }
+
+
+        embed = {
+            "username": f'{nom_webhook}',
+            "avatar_url": f"{avatar_webhook}",
+            "embeds": [
+                {
+                        "color": 0xFF0000,
+                        "title": f'Nombre de mots : {nombre}',
+                        'description': f' {resultat}',
+                        "author": {
+                            "name": "Choix de mots",
+                            'icon-url': 'https://i.imgur.com/wxf30FQ.jpg'
+                        },
+                    'thumbnail': {
+                            'url': avatar_webhook,
+                        }
+                }]}
+
+        post(lien, data=dumps(embed).encode("utf-8"), headers=headers)
+
+        return True
+
+word_in_webhook(lien='https://discord.com/api/webhooks/932310737425158164/m36x1TJytSNaCH29qqb-S53O0Q6hXsVWZS7zfLkwMot3AtjV9OmElLagPQE3IPsQ0z0e', nombre=2, nom_webhook='Mots')
